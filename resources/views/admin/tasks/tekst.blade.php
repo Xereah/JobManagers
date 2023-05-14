388df118b8e7a14b04c94a06be754d37816afab0
Xereah <patrykstruzik@onet.pl>
2023-05-14 17:30:47 +0200

    poprawne dodawanie i edycja zadań

--- resources/views/admin/tasks/tekst.blade.php
+++ resources/views/admin/tasks/tekst.blade.php
@@ -1,59 +1,3 @@
-@extends('layouts.admin2')
-@section('content')
-
-<div class="container">
-    <div id='calendar'></div>
-</div>
-<div class="modal" id="taskModal" tabindex="-1" role="dialog" aria-labelledby="taskModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="taskModalLabel">Dodaj zadanie</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="taskTitle">Tytuł zadania:</label>
-                    <input type="text" class="form-control" id="taskTitle" placeholder="Tytuł zadania">
-                </div>
-                <div class="form-group">
-                    <label for="companySelect">Wybierz firmę:</label>
-                    <select name="fk_company" id="fk_company" class="form-control select2" required>
-                        <option value=""></option>
-                        @foreach($companies as $company)
-                        <option value="{{ $company->kontrahent_id }}">{{ $company -> kontrahent_kod }}</option>
-                        @endforeach
-
-                    </select>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" id="addTaskBtn">Dodaj zadanie</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Anuluj</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-@endsection
-@section('scripts')
-
-@parent
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
-
 <script type="text/javascript">
 $(document).ready(function() {
     var SITEURL = "{{ url('/') }}";
@@ -78,15 +22,15 @@ $(document).ready(function() {
             $('#taskModal').modal('show');
         },
 
-        eventDrop: function(event) {
-            var start = event.start.format("YYYY-MM-DD");
-           
+        eventDrop: function(event, delta) {
+            var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
+            var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
             $.ajax({
                 url: SITEURL + '/fullcalenderAjax',
                 data: {
                     title: event.title,
                     start: start,
-                    end: start,
+                    end: end,
                     id: event.id,
                     type: 'update'
                 },
@@ -117,6 +61,7 @@ $(document).ready(function() {
 
         dayClick: function(date, jsEvent, view) {
             clickedDate = date.format("YYYY-MM-DD"); // Aktualizacja wartości zmiennej clickedDate
+            console.log("Clicked date:", clickedDate);
             $('#taskModal').data('clickedDate', clickedDate);
             $('#taskModal').modal('show');
         }
@@ -140,7 +85,7 @@ $(document).ready(function() {
                 },
                 type: "POST",
                 success: function (data) {
-                    displayMessage("Pomyślnie dodano zadanie");
+                    displayMessage("Event Created Successfully");
                     var eventData = {
                         id: data.id,
                         title: title,
@@ -154,7 +99,7 @@ $(document).ready(function() {
             });
             $('#taskModal').modal('hide');
         } else {
-            alert("Uzupełnij reszte pól");
+            alert("Please fill all the fields");
         }
     });
 
@@ -163,5 +108,3 @@ $(document).ready(function() {
     }
 });
 </script>
-
-@endsection
